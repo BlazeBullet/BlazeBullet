c2f5122c5f959521630edcbe1e789562217380a5
SirTenzin <jeffreypfc21@gmail.com>
2021-03-16 20:05:03 +0000

    14th | Add all stores, add plugins.js, create new repo for store.

---
+++ OpenBullet2/wwwroot/js/plugins.js
@@ -0,0 +1,62 @@
+﻿var loadplugins = function loadplugins() {
+    var github = new XMLHttpRequest();
+    github.open("GET", "https://raw.githubusercontent.com/BlazeBullet/store/master/Plugins.json", false);
+    github.send(null);
+
+    var l = JSON.parse(github.responseText);
+    var end = "";
+    var x = document.getElementsByClassName("store")[0];
+
+    l["root"].forEach(a => {
+        end +=  `
+            <tr>
+                <th scope="row">${a.name.toLocaleString()} - ${a.author}</th>
+                <td>${a.desc} - <a href="${a.download}">Download</a></td>
+            </tr>
+        `
+    })
+
+    x.innerHTML = "<table class=\"table\">" + end.toString(); + "</table>"
+}
+
+var loadthemes = function loadplugins() {
+    var github = new XMLHttpRequest();
+    github.open("GET", "https://raw.githubusercontent.com/BlazeBullet/store/master/Themes.json", false);
+    github.send(null);
+
+    var l = JSON.parse(github.responseText);
+    var end = "";
+    var x = document.getElementsByClassName("store")[0];
+
+    l["root"].forEach(a => {
+        end += `
+            <tr>
+                <th scope="row">${a.name.toLocaleString()} - ${a.author}</th>
+                <td>${a.desc} - <a href="${a.download}">Download</a></td>
+            </tr>
+        `
+    })
+
+    x.innerHTML = "<table class=\"table\">" + end.toString(); + "</table>"
+}
+
+var loadconfigs = function loadconfigs() {
+    var github = new XMLHttpRequest();
+    github.open("GET", "https://raw.githubusercontent.com/BlazeBullet/store/master/Configs.json", false);
+    github.send(null);
+
+    var l = JSON.parse(github.responseText);
+    var end = "";
+    var x = document.getElementsByClassName("store")[0];
+
+    l["root"].forEach(a => {
+        end += `
+            <tr>
+                <th scope="row">${a.name.toLocaleString()} - ${a.author}</th>
+                <td>${a.desc} - <a href="${a.download}">Download</a></td>
+            </tr>
+        `
+    })
+
+    x.innerHTML = "<table class=\"table\">" + end.toString(); + "</table>"
+}
